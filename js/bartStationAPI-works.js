fd09734a04b63b13ccaaf3ae2eb800dd94edc1c5
Christian Chandler <247webdev+github@googlemail.com>
2015-02-05 11:41:45 -0800

    got handlebars to populate a dropdown

--- js/bartStationAPI-works.js
+++ js/bartStationAPI-works.js
@@ -2,65 +2,60 @@ $(document).ready(function() {
 	var x2js = new X2JS(),
 		recursionCount = 0;
 
-	// var stations;
-
 	var getStations = function (){
-
 	$.ajax({
-			url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=QYVK-J9QW-IUDQ-DT35",  // BART's key  MW9S-E7SL-26DU-VV8V
-			type: "GET",
-			dataType: "xml",
-			success: function( xml ) {
-						console.log( "success ", xml);
-						var bart = x2js.xml2json(xml);
+		url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=QYVK-J9QW-IUDQ-DT35",  // BART's key  MW9S-E7SL-26DU-VV8V
+		type: "GET",
+		dataType: "xml",
+		success: function( data ) {
+			console.log( "success ", data);
+			var bartData = x2js.xml2json( data );
 
-						if(bart.root.message === ""){
-							$('#data').text(bart.root.stations.station[1].name);
-						}else{
-							console.log("We got the error");
-							recursionCount++;
-							if(recursionCount < 5){
-								return getStations();
-							} else {
-								console.log("Too many API recursions!!");
-								recursionCount = 0;
-							}
-						}
+			if(bartData.root.message === ""){
+// API returned non-error
+				console.log("moving forward... no error");
 
-						
+				// $('#data').text(bartData.root.stations.station[1].name);
 
+				var source = $('#staionList-template').html();
+				var template = Handlebars.compile(source);
+				var html = template({stationData: bartData});
+				$('.container').html(html);
 
-						// console.log( "success ", xml.root );
-						// return xml;
-						// stations = xml;
-						// return stations;
+			}else{
+// API returned error message
+				console.log("We got the error");
+				recursionCount++;
+				if(recursionCount < 5){
+					return getStations();
 
-						// console.log( toJSON( xml ) );
-						// return toJSONasVAR( xml );
-						// toJSONasVAR( xml );
+				} else {
+// there were too many recursions... break recursion!!
+					console.log("Too many API recursions!!");
+					recursionCount = 0;
+				} // end else where recursionCount = 5
+			} // end else where bartData.root.message not ""
+		}, // end success handler
 
-					}, // end success
-			fail: function() {
-					    alert( "getStations failed\n" );
-					}, // end fail
-			error: function( xhr, status, errorThrown ) {
-						alert( "There was a problem!" );
-						console.log( "Error: " + errorThrown + "\n\n");
-						console.log( "Status: " + status );
-						console.dir( xhr );
-					}, // end error
-			always: function() {
-					    alert( "getStations completed" );
-					} // end always
-			}
+		fail: function() {
+		    alert( "getStations failed\n" );
+		}, // end fail handler
 
-			// console.log( stations );
+		error: function( xhr, status, errorThrown ) {
+			alert( "There was a problem!" );
+			console.log( "Error: " + errorThrown + "\n\n");
+			console.log( "Status: " + status );
+			console.dir( xhr );
+		}, // end error handler
 
-		); // end ajax
+		always: function() {
+		    alert( "getStations completed" );
+		} // end always handler
+
+		} // end ???
+
+	); // end ajax
 	}; //end getStations function
-
-
-
 
 getStations();
 
