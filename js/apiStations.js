d0bbf63746b4f8e61d866c855b2c2aa0bbba870e
Christian Chandler <247webdev+github@googlemail.com>
2015-02-05 19:24:47 -0800

    got the bartData obj to persist and can go back into it and get the train time

--- js/apiStations.js
+++ js/apiStations.js
@@ -1,17 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function(){
 	var x2js = new X2JS(),
 		user = {
-			"station":"STATION!",
-			"route":"ROUTE!!",
-			"leadTime":"LEADTIME!!!"
+			"station":"",
+			"stationIndex":"", // not needed
+			"route":"",
+			"routeIndex":"",
+			"leadTime":"",
+			"trainObj":""
 		},
+		bartData,
 		recursionCount = 0,
 		apiKeyIndex = 0,
 		apiKeys = ["QYVK-J9QW-IUDQ-DT35", "QSVS-UI6Y-ILTQ-DT35", "MW9S-E7SL-26DU-VV8V"], // QYVK... and QSVS... are mine, MW9S... is BART's
 		currentKey = apiKeys[ apiKeyIndex ];
 
 // initial function for ajax call to BART to get a list of stations
-	var getStations = function (){
+	var getStations = function(){
 	$.ajax({
 		url: "http://api.bart.gov/api/stn.aspx?cmd=stns",
 		type: "GET",
@@ -54,7 +58,7 @@ $(document).ready(function() {
 			} // end else where bartData.root.message not ""
 		}, // end success handler
 
-		fail: function() {
+		fail: function(){
 		    alert( "getStations failed\n" );
 		}, // end fail handler
 
@@ -65,7 +69,7 @@ $(document).ready(function() {
 			console.dir( xhr );
 		}, // end error handler
 
-		always: function() {
+		always: function(){
 		    alert( "getStations completed" );
 		} // end always handler
 
@@ -76,18 +80,18 @@ $(document).ready(function() {
 
 
 // function for ajax call to BART to get a list of train routes for a given station
-	var getRoutes = function ( stationSelected ){
+	var getRoutes = function(){
 	$.ajax({
 		url: "http://api.bart.gov/api/etd.aspx?cmd=etd",
 		type: "GET",
 		dataType: "xml",
 		data: {
 			key: currentKey,
-			orig: stationSelected
+			orig: user.station
 		},
 		success: function( data ) {
 // console.log( "success ", data);
-			var bartData = x2js.xml2json( data );
+			bartData = x2js.xml2json( data );
 
 			if(bartData.root.message === ""){
 // API returned non-error
@@ -140,6 +144,15 @@ console.log( bartData );
 	); // end ajax
 	}; //end getRoutes function
 
+	var getTime = function(){
+// console.log("bartData: " + bartData + "user.routeIndex: " + user.routeIndex );
+console.log("Get train time: " + bartData.root.station.etd[1].estimate[0].minutes );
+	// user.root.station.etd[0].abbreviation
+	// root.station.etd[i].abbreviation
+		// 				  .destination
+		// 				  .estimate[i]
+		// 						.minutes 
+	};
 
 
 getStations();
@@ -148,30 +161,34 @@ getStations();
 // listener for the user's choice of a station
 $('body').on('change', '#chooseStation', function( e ){
 	user.station = $("#chooseStation").val();
+	user.stationIndex = $("#chooseStation")[0].selectedIndex -1;
+// console.log( $("#chooseStation")[0].selectedIndex -1 ); // match this value against the Inspect Element in Concole data-index
+// console.log( "\nstation index: " + user.stationIndex );
+// console.log( "\nStation Value: " + user.station );
 
-	console.log("\nStation Value: " + user.station );
-
-	getRoutes( user.station );
+	getRoutes();
 });
 
 // listener for the user's choice of a train line
 $('body').on('change', '#chooseRoute', function( e ){
 	user.route = $("#chooseRoute").val();
+	user.routeIndex = $("#chooseRoute")[0].selectedIndex -1;
 
-	console.log("\nRoute Value: " + user.route );
+// console.log("\nRoute Index: " + user.routeIndex);
+// console.log("\nRoute Value: " + user.route );
 });
 
 // listener for the user's choice of a lead walking time
 $('body').on('change', '#chooseWalkTime', function( e ){
 	user.leadTime = $("#chooseWalkTime").val();
 
-	console.log("\nLead Time Value: " + user.leadTime );
+	// console.log("\nLead Time Value: " + user.leadTime );
 });
 
 // listener for the button which calls the getTime() funct
 $('#getCountdownTime').on('click', function( e ){
-	console.log( user );
-	// getTime( user );
+	// console.log( user );
+	getTime();
 });
 
 // console.log("\nstation: " + user.station +
